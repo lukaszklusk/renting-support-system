cfcdadb152550fefe905bb5f4febabb7ba145bec
Łukasz Kluska <lukaszklusk@gmail.com>
2023-05-22 00:12:11 +0200

    Initialize project

---
+++ src/main/java/pl/edu/agh/student/rentsys/RentsysApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.student.rentsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/agh/student/rentsys/RentsysApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.student.rentsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
