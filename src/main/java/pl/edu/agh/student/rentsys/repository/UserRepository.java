ee45db3ec24cecc3d217f1d1f1860104c8b03651
Łukasz Kluska <lukaszklusk@gmail.com>
2023-05-23 00:14:08 +0200

    Basic repository/service/controller structure

---
+++ src/main/java/pl/edu/agh/student/rentsys/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.student.rentsys.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.edu.agh.student.rentsys.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/pl/edu/agh/student/rentsys/service/UserService.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.student.rentsys.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
