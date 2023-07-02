f6982fd4da7e87adf30094bec534e9790baaea59
miskowic <miskowicz@student..agh.edu.pl>
2023-07-02 16:57:48 +0200

    add bold text for active link in navbar

--- frontend/src/components/common/AppNavbar.js
+++ frontend/src/components/common/AppNavbar.js
@@ -1,17 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import { ROLES } from "../../config/roles";
 import useLogout from "../../hooks/useLogout";
 
 const AppNavbar = () => {
+  const location = useLocation();
   const { auth } = useAuth();
   const isLoggedIn = auth.isLoggedIn;
   const isClient = isLoggedIn && auth.roles?.includes(ROLES.client);
   const isOwner = isLoggedIn && auth.roles?.includes(ROLES.owner);
   const isAdmin = isLoggedIn && auth.roles?.includes(ROLES.admin);
   const logout = useLogout();
+
+  const [activeLink, setActiveLink] = useState("");
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+    currentPath.length > 0 && setActiveLink(currentPath.substring(1));
+  }, [location]);
 
   return (
     <Navbar bg="dark" variant="dark" expand="sm">
@@ -23,7 +31,13 @@ const AppNavbar = () => {
         <Nav>
           {isLoggedIn ? (
             <>
-              <Nav.Link as={Link} to="dashboard" className="custom-navbar-link">
+              <Nav.Link
+                as={Link}
+                to="dashboard"
+                className={`custom-navbar-link ${
+                  activeLink === "dashboard" ? "active" : ""
+                }`}
+              >
                 Dashboard
               </Nav.Link>
 
@@ -32,14 +46,18 @@ const AppNavbar = () => {
                   <Nav.Link
                     as={Link}
                     to="/apartment"
-                    className="custom-navbar-link"
+                    className={`custom-navbar-link ${
+                      activeLink === "apartment" ? "active" : ""
+                    }`}
                   >
                     Apartment
                   </Nav.Link>
                   <Nav.Link
                     as={Link}
                     to="/agreement"
-                    className="custom-navbar-link"
+                    className={`custom-navbar-link ${
+                      activeLink === "agreement" ? "active" : ""
+                    }`}
                   >
                     Agreement
                   </Nav.Link>
@@ -51,21 +69,27 @@ const AppNavbar = () => {
                   <Nav.Link
                     as={Link}
                     to="/apartments"
-                    className="custom-navbar-link"
+                    className={`custom-navbar-link ${
+                      activeLink === "apartments" ? "active" : ""
+                    }`}
                   >
                     Apartments
                   </Nav.Link>
                   <Nav.Link
                     as={Link}
                     to="/agreements"
-                    className="custom-navbar-link"
+                    className={`custom-navbar-link ${
+                      activeLink === "agreements" ? "active" : ""
+                    }`}
                   >
                     Agreements
                   </Nav.Link>
                   <Nav.Link
                     as={Link}
                     to="/reports"
-                    className="custom-navbar-link"
+                    className={`custom-navbar-link ${
+                      activeLink === "reports" ? "active" : ""
+                    }`}
                   >
                     Reports
                   </Nav.Link>
@@ -75,7 +99,9 @@ const AppNavbar = () => {
               <Nav.Link
                 as={Link}
                 to="sign-in"
-                className="custom-navbar-link"
+                className={`custom-navbar-link ${
+                  activeLink === "sign-in" ? "active" : ""
+                }`}
                 onClick={() => {
                   logout();
                 }}
@@ -85,19 +111,49 @@ const AppNavbar = () => {
             </>
           ) : (
             <>
-              <Nav.Link as={Link} to="/" className="custom-navbar-link">
+              <Nav.Link
+                as={Link}
+                to="/"
+                className={`custom-navbar-link ${
+                  activeLink === "" ? "active" : ""
+                }`}
+              >
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/about" className="custom-navbar-link">
+              <Nav.Link
+                as={Link}
+                to="/about"
+                className={`custom-navbar-link ${
+                  activeLink === "about" ? "active" : ""
+                }`}
+              >
                 About
               </Nav.Link>
-              <Nav.Link as={Link} to="/contact" className="custom-navbar-link">
+              <Nav.Link
+                as={Link}
+                to="/contact"
+                className={`custom-navbar-link ${
+                  activeLink === "contact" ? "active" : ""
+                }`}
+              >
                 Contact
               </Nav.Link>
-              <Nav.Link as={Link} to="/sign-up" className="custom-navbar-link">
+              <Nav.Link
+                as={Link}
+                to="/sign-up"
+                className={`custom-navbar-link ${
+                  activeLink === "sign-up" ? "active" : ""
+                }`}
+              >
                 Sign Up
               </Nav.Link>
-              <Nav.Link as={Link} to="/sign-in" className="custom-navbar-link">
+              <Nav.Link
+                as={Link}
+                to="/sign-in"
+                className={`custom-navbar-link ${
+                  activeLink === "sign-in" ? "active" : ""
+                }`}
+              >
                 Sign In
               </Nav.Link>
             </>
