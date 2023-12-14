9bf68c20dfc2d2e5f1c585881825b92f04b0534f
Łukasz Kluska <lukaszklusk@gmail.com>
2023-12-14 22:08:23 +0100

    Scheduler notification creation

--- backend/src/main/java/pl/edu/agh/student/rentsys/scheduler/SchedulerConfig.java
+++ backend/src/main/java/pl/edu/agh/student/rentsys/scheduler/SchedulerConfig.java
@@ -3,8 +3,10 @@ package pl.edu.agh.student.rentsys.scheduler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import pl.edu.agh.student.rentsys.model.NotificationType;
 import pl.edu.agh.student.rentsys.model.Payment;
 import pl.edu.agh.student.rentsys.model.PaymentStatus;
+import pl.edu.agh.student.rentsys.service.NotificationService;
 import pl.edu.agh.student.rentsys.service.PaymentService;
 
 import java.time.LocalDate;
@@ -18,8 +20,11 @@ public class SchedulerConfig {
 
     private final PaymentService paymentService;
 
-    public SchedulerConfig(PaymentService paymentService) {
+    private final NotificationService notificationService;
+
+    public SchedulerConfig(PaymentService paymentService, NotificationService notificationService) {
         this.paymentService = paymentService;
+        this.notificationService = notificationService;
     }
 
     @Scheduled(cron = "0 0 3 * * *")
@@ -29,10 +34,18 @@ public class SchedulerConfig {
                 new PaymentStatus[]{PaymentStatus.future, PaymentStatus.due});
         for(Payment p: payments){
             if(p.getStatus().equals(PaymentStatus.due)){
-                if(p.getDueDate().isBefore(LocalDate.now())) p.setStatus(PaymentStatus.overdue);
+                if(p.getDueDate().isBefore(LocalDate.now())) {
+                    p.setStatus(PaymentStatus.overdue);
+                    notificationService.createAndSendNotification(
+                            p.getAgreement(), NotificationType.payment_due, p.getAgreement().getApartment().getName());
+                }
             }
             if(p.getStatus().equals(PaymentStatus.future)){
-                if(DAYS.between(LocalDate.now(),p.getDueDate()) <= 30) p.setStatus(PaymentStatus.due);
+                if(DAYS.between(LocalDate.now(),p.getDueDate()) <= 30){
+                    p.setStatus(PaymentStatus.due);
+                    notificationService.createAndSendNotification(
+                            p.getAgreement(), NotificationType.payment_overdue, p.getAgreement().getApartment().getName());
+                }
             }
         }
         paymentService.updatePayments(payments);
