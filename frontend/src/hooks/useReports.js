bc87a11bee9321930f48e05db27a26031601737f
miskowic <miskowicz@student..agh.edu.pl>
2023-12-31 00:32:43 +0100

    update reports view

--- frontend/src/hooks/useReports.js
+++ frontend/src/hooks/useReports.js
@@ -4,13 +4,35 @@ const getBaseUrl = (username) => {
   return `/user/${username}/report`;
 };
 
-export const useUserReport = () => {
+export const useUserOverviewReport = () => {
   const getData = useGetRequest();
 
-  const getUserReport = async (username) => {
-    const url = getBaseUrl(username);
+  const getUserOverviewReport = async (username) => {
+    const url = `${getBaseUrl(username)}/overview`;
     console.log("url:", url);
     return getData(url, { responseType: "blob" });
   };
-  return getUserReport;
+  return getUserOverviewReport;
+};
+
+export const useUserSimpleReport = () => {
+  const getData = useGetRequest();
+
+  const getUserSimpleReport = async (username) => {
+    const url = `${getBaseUrl(username)}/simple`;
+    console.log("url:", url);
+    return getData(url, { responseType: "blob" });
+  };
+  return getUserSimpleReport;
+};
+
+export const useUserDetailedReport = () => {
+  const getData = useGetRequest();
+
+  const getUserDetailedReport = async (username) => {
+    const url = `${getBaseUrl(username)}/detailed`;
+    console.log("url:", url);
+    return getData(url, { responseType: "blob" });
+  };
+  return getUserDetailedReport;
 };
